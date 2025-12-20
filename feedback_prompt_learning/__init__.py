4f79bcc010f8398c9ca1bec626d03a2eaa6e186c
chingis <chingisoinar@gmail.com>
2025-12-20 10:03:18 +0900

    feat: config amanager

--- feedback_prompt_learning/__init__.py
+++ feedback_prompt_learning/__init__.py
@@ -1,21 +1,9 @@
 __version__ = "0.1.0"
-from hydra import compose, initialize
-from omegaconf import OmegaConf
-
+from feedback_prompt_learning.config import ConfigManager
 from feedback_prompt_learning.search_algo import *  # noqa: F401, F403
 
-
-def load_config(verbose=False):
-    """Load configuration using Hydra"""
-    # Use relative path to configs directory (relative to package root)
-    with initialize(version_base="1.3.2", config_path="./configs"):
-        cfg = compose(config_name="config", overrides=["optimizer=mcts"])
-        if verbose:
-            print(OmegaConf.to_yaml(cfg))
-        return cfg
-
-
-cfg = load_config()
+config = ConfigManager()
+config.load()
 
 __all__ = [
     "__version__",
