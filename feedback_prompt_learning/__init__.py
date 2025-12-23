384b1c61aa250a966db136cdd2ed253e419289dd
chingis <chingisoinar@gmail.com>
2025-12-23 13:02:01 +0900

    refactor: remove unusued things in init

--- feedback_prompt_learning/__init__.py
+++ feedback_prompt_learning/__init__.py
@@ -4,11 +4,3 @@ from feedback_prompt_learning.search_algo import *  # noqa: F401, F403
 
 config = ConfigManager()
 config.load()
-
-__all__ = [
-    "__version__",
-    "__author__",
-    "__email__",
-    "cfg",
-    "load_config",
-]
